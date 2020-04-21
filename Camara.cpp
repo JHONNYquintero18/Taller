3c6fb89eccc50b96f276b93212af0e893f26e83e
jhonny quintero <63802122+JHONNYquintero18@users.noreply.github.com>
2020-04-21 14:13:54 -0500

    jhonny quintero
    modificacion de trabajo grupal

--- Camara.cpp
+++ Camara.cpp
@@ -1,38 +1,24 @@
-/*"	Realizar un ejercicio en donde se muestre un menú que diga ingreso a la cámara detectora 
-del segundo síntoma del covid-19.*/
+/*"	Realizar un ejercicio en donde se muestre un menÃº que diga ingreso a la cÃ¡mara detectora 
+del segundo sÃ­ntoma del covid-19.*/
 
 #include <iostream>
-
+#include <math.h>
 using namespace std;
-
-int main (){
-	
-	int temperature, tos;
-	cout<<"\nCAMARA DETECTORA DE CORONAVIRUS\n\n";
-	
-	cout<<"\nDigita tu temperatura: "; cin>> temperature;
-	
-	
+main ()
+{
+	double temperatura ;
+	cout<<" CAMARA DETECTORA DEL SEGUNDO SINTOMA AL COVID-19 "<<endl<<endl;
+	cout<<" recuerda que si tienes una temperatura mayor a 36 grados c es momento de buscar ayuda profesional  "<<endl<<endl; cout <<"digite su temperatura actual por favor: "<<endl;
+	cin>>temperatura;
+	if(temperatura>36)
+{
+	cout<<" sintoma de alerta necesitas ayuda de un doctor "<<endl;
+	cout<<" recuerda primero pedir ayuda a la linea de atencion #123-4 ";
+	}	
+	else
 	{
-		if(temperature<37)
-		cout<<" \n\nSINTOMA NEGATIVO: No necesitas un chequeo medico.\n ";	
-	
-		else
-		
-		cout<<"\nSINTOMA POSITIVO: URGENTE Necesitas un chequeo medico\n";
+		cout<<" aun estas a tiempo para no contagiarte "<<endl;
+		cout<<" recuerda lavarte las manos  cada 2 horas ";
 	}
-		cout<<"\nSi tienes tos digita 1, de lo contrario digita 2: "; cin>> tos;
-	{
-		if(tos!=1)
-		cout<<" \n\nSINTOMA NEGATIVO: No necesitas un chequeo medico.\n ";	
-	
-		else
-		
-		cout<<"\nSINTOMA POSITIVO: URGENTE Necesitas un chequeo medico\n";
-	}
-		cout<<"\nPara terminar digita 0.\n ";
-		cin>>temperature;
-	
-		
-	return 0;
+return 0;	
 }
